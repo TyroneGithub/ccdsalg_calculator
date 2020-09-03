f4d515e60ddbccf04d1e91ab0d9261da7068dc57
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-09-03 14:37:01 +0800

    Update infixToPostFix.c

--- infixToPostFix.c
+++ infixToPostFix.c
@@ -26,55 +26,61 @@ void infixToPostfix(Stack *stack, char input[], char output[])
     for (i = 0; i < strlen(input); i++)
     {
         //   printf("%lld Evaluating index %d\n", strlen(input), i);
-        if (input[i] != ' ')
-        {
-            if (isdigit(input[i]))
-            {
-                //                printf("input %c is an operand \n", input[i]);
-                output[char_count++] = input[i];
-            }
-            else if (isOperator(input[i]))
-            {
-                //              printf("input %c is an operator \n", input[i]);
-                //printf(" % c % c ", input[i], top(stack));
 
-                if (stack_empty(stack) || top(stack) == '(' || (isOperator(input[i]) > isOperator(top(stack)) || isOperator(input[i]) == 5))
-                    push_stack(stack, input[i]);
+        if (isdigit(input[i]))
+        {
+            //                printf("input %c is an operand \n", input[i]);
+            if (i != 0)
+            {
+                if (isdigit(input[i - 1]))
+                    output[char_count++] = input[i];
                 else
                 {
-                    while (isOperator(input[i]) <= isOperator(top(stack)) && !stack_empty(stack))
-                    {
-                        output[char_count++] = pop_stack(stack);
-                        output[char_count++] = ' ';
-                    }
-
-                    push_stack(stack, input[i]);
+                    output[char_count++] = ' ';
+                    output[char_count++] = input[i];
                 }
             }
-            else if (input[i] == '(')
+            else
             {
+                output[char_count++] = input[i];
+            }
+        }
+        else if (isOperator(input[i]))
+        {
+            //              printf("input %c is an operator \n", input[i]);
+            //printf(" % c % c ", input[i], top(stack));
+
+            if (stack_empty(stack) || top(stack) == '(' || (isOperator(input[i]) > isOperator(top(stack)) || isOperator(input[i]) == 5))
+                push_stack(stack, input[i]);
+            else
+            {
+                while (isOperator(input[i]) <= isOperator(top(stack)) && !stack_empty(stack))
+                {
+                    output[char_count++] = ' ';
+                    output[char_count++] = pop_stack(stack);
+                }
+
                 push_stack(stack, input[i]);
             }
-            else if (input[i] == ')')
-            {
-                while (top(stack) != '(')
-                {
-                    output[char_count++] = pop_stack(stack);
-                    output[char_count++] = ' ';
-                }
-                pop_stack(stack);
-            }
         }
-        else
+        else if (input[i] == '(')
         {
-            if (char_count && isdigit(output[char_count - 1]))
-                output[char_count++] = ' ';
+            push_stack(stack, input[i]);
+        }
+        else if (input[i] == ')')
+        {
+            while (top(stack) != '(')
+            {
+                output[char_count++] = pop_stack(stack);
+            }
+            pop_stack(stack);
         }
     }
+
     while (!stack_empty(stack))
     {
-        output[char_count++] = pop_stack(stack);
         output[char_count++] = ' ';
+        output[char_count++] = pop_stack(stack);
     }
     output[char_count] = '\0';
 }
